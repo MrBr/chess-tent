353c88911b99a6ec752a9f0bc3f8962ad84b3cd5
Luka Bracanovic <lbracano@gmail.com>
2020-08-25 18:06:50 +0200

    Limit chunks count

--- packages/web/craco.config.js
+++ packages/web/craco.config.js
@@ -1,4 +1,5 @@
 const CracoAlias = require('craco-alias');
+const webpack = require('webpack');
 
 module.exports = {
   plugins: [
@@ -11,6 +12,20 @@ module.exports = {
         baseUrl: './src',
         // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
         tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.plugins.push(
+            new webpack.optimize.LimitChunkCountPlugin({
+              maxChunks: 1,
+            }),
+          );
+
+          // Always return the config object.
+          return webpackConfig;
+        },
       },
     },
   ],

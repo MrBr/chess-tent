60e9bc8c04a68a541f9ffaea77a9f1a2a51a2e90
Luka Bracanovic <lbracano@gmail.com>
2020-11-04 21:34:14 +0100

    Fix Avatar onClick

--- packages/web/src/modules/ui/Img.tsx
+++ packages/web/src/modules/ui/Img.tsx
@@ -28,11 +28,13 @@ const sizeEnhancer = (props: ComponentProps<UI['Avatar']>) => {
   };
 };
 
-export const Avatar = styled(({ src, name, className }) => {
+export const Avatar = styled(({ src, name, className, onClick }) => {
   return src ? (
-    <img className={className} src={src} alt={name} />
+    <img className={className} src={src} alt={name} onClick={onClick} />
   ) : (
-    <div className={className}>{name?.[0] || 'A'}</div>
+    <div className={className} onClick={onClick}>
+      {name?.[0] || 'A'}
+    </div>
   );
 })(
   {

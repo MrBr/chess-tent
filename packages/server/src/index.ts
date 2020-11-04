eb3b618d741a19b1834f30d82704453d64e9de02
Luka Bracanovic <lbracano@gmail.com>
2020-11-04 21:23:31 +0100

    Remove static file from server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,8 +2,8 @@ const { addAlias } = require("module-alias");
 addAlias("@application", __dirname + "/application");
 addAlias("@types", __dirname + "/application");
 require("module-alias/register");
-const path = require('path');
-require("dotenv").config({ path: path.join(__dirname, '../.env') });
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 require("./modules");
 const application = require("./application").default;

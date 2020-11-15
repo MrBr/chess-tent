0ca4ab1f6edac73809bec4184bb8748d6ca6bd38
alen-sljivar <alen.sljivar@five.agency>
2020-11-15 18:37:54 +0100

    add Login button

--- packages/web/src/modules/user/pages/login.tsx
+++ packages/web/src/modules/user/pages/login.tsx
@@ -62,78 +62,70 @@ export default () => {
   }
 
   return (
-    // <Modal
-    //   show
-    //   dialogClassName="full-screen-dialog"
-    //   onEscapeKeyDown={() => history.goBack()}
-    // >
-    <>
-      <Container className="h-100 d-flex justify-content-center align-items-center no-gutters mx-auto">
-        <Col className="col-xl-6 col-lg-8 col-md-10 col-sm-12">
-          <Card className="px-5 py-4 border rounded-lg">
-            <Row className="d-flex justify-content-between align-items-center">
-              <Col className="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-start">
-                <Headline1>Sign In</Headline1>
-              </Col>
-              <Col className="col-12 col-sm-6 d-flex flex-nowrap justify-content-center justify-content-sm-end">
-                <NoWrapText>Don’t have an account?</NoWrapText>
-                <NoWrapText className="font-weight-bold ml-1">
-                  <Link to="/register">
-                    <u>Sign Up</u>
-                  </Link>
-                </NoWrapText>
-              </Col>
-            </Row>
-            <Form
-              initialValues={{
-                email: '',
-                password: '',
-              }}
-              validationSchema={LoginSchema}
-              onSubmit={({ ...user }) => fetch(user)}
-            >
-              <FormGroup className="pt-4">
-                <Form.Input
-                  size="lg"
-                  type="email"
-                  name="email"
-                  placeholder="Email address"
-                />
-              </FormGroup>
-              <FormGroup className="pt-3">
-                <Form.Input
-                  size="lg"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-              </FormGroup>
-              <FormGroup className="w-100 d-flex justify-content-between align-items-center mt-4">
-                <Row className="w-100" noGutters>
-                  <Col className="col-12 col-sm-6 order-2 order-sm-1 d-flex align-items-center justify-content-center justify-content-sm-start">
-                    <Text fontSize="small" className="text-center text-sm-left">
-                      <u>Forgot password?</u>
-                    </Text>
-                  </Col>
-                  <Col className="col-12 col-sm-6 order-1 order-sm-2 d-flex justify-content-center justify-content-sm-end">
-                    <LoginButton
-                      type="submit"
-                      disabled={loading}
-                      className="mb-3"
-                    >
-                      Login
-                    </LoginButton>
-                  </Col>
-                </Row>
-              </FormGroup>
-            </Form>
-          </Card>
-        </Col>
-      </Container>
+    <Container className="h-100 d-flex justify-content-center align-items-center no-gutters mx-auto">
+      <Col className="col-xl-6 col-lg-8 col-md-10 col-sm-12">
+        <Card className="px-5 py-4 border rounded-lg">
+          <Row className="d-flex justify-content-between align-items-center">
+            <Col className="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-start">
+              <Headline1>Sign In</Headline1>
+            </Col>
+            <Col className="col-12 col-sm-6 d-flex flex-nowrap justify-content-center justify-content-sm-end">
+              <NoWrapText>Don’t have an account?</NoWrapText>
+              <NoWrapText className="font-weight-bold ml-1">
+                <Link to="/register">
+                  <u>Sign Up</u>
+                </Link>
+              </NoWrapText>
+            </Col>
+          </Row>
+          <Form
+            initialValues={{
+              email: '',
+              password: '',
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={({ ...user }) => fetch(user)}
+          >
+            <FormGroup className="pt-4">
+              <Form.Input
+                size="lg"
+                type="email"
+                name="email"
+                placeholder="Email address"
+              />
+            </FormGroup>
+            <FormGroup className="pt-3">
+              <Form.Input
+                size="lg"
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </FormGroup>
+            <FormGroup className="w-100 d-flex justify-content-between align-items-center mt-4">
+              <Row className="w-100" noGutters>
+                <Col className="col-12 col-sm-6 order-2 order-sm-1 d-flex align-items-center justify-content-center justify-content-sm-start">
+                  <Text fontSize="small" className="text-center text-sm-left">
+                    <u>Forgot password?</u>
+                  </Text>
+                </Col>
+                <Col className="col-12 col-sm-6 order-1 order-sm-2 d-flex justify-content-center justify-content-sm-end">
+                  <LoginButton
+                    type="submit"
+                    disabled={loading}
+                    className="mb-3"
+                  >
+                    Login
+                  </LoginButton>
+                </Col>
+              </Row>
+            </FormGroup>
+          </Form>
+        </Card>
+      </Col>
       <Absolute left={25} top={15} onClick={() => history.goBack()}>
         <Icon type="close" size="large" />
       </Absolute>
-    </>
-    //  </Modal>
+    </Container>
   );
 };

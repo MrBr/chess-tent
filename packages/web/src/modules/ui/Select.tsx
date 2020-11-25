0b92cfbfc646ec5c3f439b4fccfd91d90860bd7d
alen-sljivar <alen.sljivar@five.agency>
2020-11-25 21:38:54 +0100

    update Select style

--- packages/web/src/modules/ui/Select.tsx
+++ packages/web/src/modules/ui/Select.tsx
@@ -1,6 +1,22 @@
+import styled from '@emotion/styled';
 import { default as RSelect, components } from 'react-select';
+// import { Select } from 'react-select/base';
 
 export const Option = components.Option;
-const Select = RSelect;
+
+const Select = (styled(RSelect)({
+  '.select__control': {
+    backgroundColor: '#F3F4F5',
+    borderRadius: 10,
+  },
+  '.select__indicator-separator': {
+    display: 'none',
+  },
+}) as unknown) as typeof RSelect;
+
+Select.defaultProps = {
+  //@ts-ignore
+  classNamePrefix: 'select',
+};
 
 export default Select;

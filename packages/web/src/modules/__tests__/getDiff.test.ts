7554fccf5cb857ad83ba5b2f6c66c2f18fdcda0b
alen-sljivar <alen.sljivar@five.agency>
2021-01-23 18:47:42 +0100

    update tests

--- packages/web/src/modules/__tests__/getDiff.test.ts
+++ packages/web/src/modules/__tests__/getDiff.test.ts
@@ -2,21 +2,49 @@ import { getDiff } from '../utils/utils';
 
 type TestSubject = { [key: string]: unknown };
 
-test("should return { 'newObjectOldString:{} 'newArrayOldNumber: []', newObjectNotExistInArray: {} } ", () => {
-  const newSubject: TestSubject = {
-    newObjectOldString: {},
-    newArrayOldNumber: [],
-    newObjectNotExistInOld: {},
-  };
+describe('getDiff', () => {
+  test("should return { 'newObjectOldString:{} 'newArrayOldNumber: []', newObjectNotExistInArray: {} } ", () => {
+    const newSubject: TestSubject = {
+      newObjectOldString: {},
+      newArrayOldNumber: [],
+      newObjectNotExistInOld: {},
+    };
 
-  const oldSubject: TestSubject = {
-    newObjectOldString: 'string',
-    newArrayOldNumber: 1,
-  };
+    const oldSubject: TestSubject = {
+      newObjectOldString: 'string',
+      newArrayOldNumber: 1,
+    };
 
-  expect(getDiff(oldSubject, newSubject, {})).toStrictEqual({
-    newObjectOldString: {},
-    newArrayOldNumber: [],
-    newObjectNotExistInOld: {},
+    expect(getDiff(oldSubject, newSubject, {})).toStrictEqual({
+      newObjectOldString: {},
+      newArrayOldNumber: [],
+      newObjectNotExistInOld: {},
+    });
+  });
+  test("should return { newObjectOldUndefined: { something: 'something'}}", () => {
+    const newSubject: TestSubject = {
+      newObjectOldUndefined: {
+        something: 'something',
+      },
+    };
+
+    const oldSubject: TestSubject = {
+      newObjectOldUndefined: undefined,
+    };
+
+    expect(getDiff(oldSubject, newSubject, {})).toStrictEqual(newSubject);
+  });
+  test('should return { newUndefinedOldObject: undefined}', () => {
+    const newSubject: TestSubject = {
+      newUndefinedOldObject: undefined,
+    };
+
+    const oldSubject: TestSubject = {
+      newUndefinedOldObject: {
+        something: 'something',
+      },
+    };
+
+    expect(getDiff(oldSubject, newSubject, {})).toStrictEqual(newSubject);
   });
 });

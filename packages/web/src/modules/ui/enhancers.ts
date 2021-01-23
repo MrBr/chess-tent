85175375ae9cfa3b45a79bf9cda1843c31855d72
alen-sljivar <alen.sljivar@five.agency>
2021-01-23 21:40:33 +0100

    extract sizeEnhancer into ui/enhancers

--- packages/web/src/modules/ui/enhancers.ts
+++ packages/web/src/modules/ui/enhancers.ts
@@ -1,3 +1,6 @@
+import { ComponentProps } from 'react';
+import { UI } from '@types';
+
 export interface BorderRadiusProps {
   borderRadius: 'large' | 'regular' | 'small' | 'extra-small';
 }
@@ -21,3 +24,31 @@ export const getBorderRadiusSize = (
 export const borderRadiusEnhancer = (props: BorderRadiusProps) => ({
   borderRadius: getBorderRadiusSize(props.borderRadius),
 });
+
+export const sizeEnhancer = (
+  props: ComponentProps<UI['Dropdown']['Toggle']>,
+) => {
+  switch (props.size) {
+    case 'extra-small':
+      return {
+        fontSize: 12,
+        padding: '2px 8px 3px 8px',
+        lineHeight: '19px',
+        borderRadius: 4,
+      };
+    case 'small':
+      return {
+        borderRadius: 6,
+        fontSize: 14,
+        lineHeight: '16px',
+        padding: '7px 12px 8px 12px',
+      };
+    case 'regular':
+    default:
+      return {
+        fontSize: 16,
+        borderRadius: 10,
+        padding: '15px 16px 14px 16px',
+      };
+  }
+};

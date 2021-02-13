69a887bb4b0001436166a0a26894085a891f446b
alen-sljivar <alen.sljivar@five.agency>
2021-02-13 20:16:19 +0100

    add setupProxy.js

---
+++ packages/web/src/setupProxy.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = app => {
+  app.use(createProxyMiddleware('/api', { target: 'http://localhost:3007' }));
+  app.use(
+    createProxyMiddleware('/api/socket.io', {
+      target: 'http://localhost:3007',
+      ws: true,
+    }),
+  );
+};

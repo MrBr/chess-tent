24e0af1d3e44af4b73fbf65c5d443bfc16f3b079
Luka Bracanovic <lbracano@gmail.com>
2021-05-04 10:10:57 +0200

    Implement createRecordService

--- packages/web/src/modules/state-records/services.ts
+++ packages/web/src/modules/state-records/services.ts
@@ -1,7 +1,33 @@
-import { RecordMeta, RecordValue } from '@chess-tent/types';
+import {
+  GetRecordNormalizedValue,
+  RecordMeta,
+  RecordValue,
+} from '@chess-tent/types';
+import { RecordService } from '@types';
+import { MiddlewareAPI } from 'redux';
 import { useRecord } from './hooks';
+import { updateRecordAction, updateRecordValueAction } from './state/actions';
+import { selectRecord } from './state/selectors';
 
 export const createRecordHook = <T extends RecordValue>(
   recordKey: string,
   type: RecordMeta['type'],
 ) => () => useRecord<T>(recordKey, type);
+
+export const createRecordService = <T extends RecordValue>(
+  recordKey: string,
+) => (store: MiddlewareAPI): RecordService<T> => {
+  const record = selectRecord<T>(recordKey)(store.getState());
+
+  const updateValue = (value: GetRecordNormalizedValue<T>) =>
+    store.dispatch(updateRecordValueAction(recordKey, value, record.meta.type));
+
+  const update = (entity: RecordValue) =>
+    store.dispatch(updateRecordAction(recordKey, entity));
+
+  return {
+    record,
+    update,
+    updateValue,
+  };
+};

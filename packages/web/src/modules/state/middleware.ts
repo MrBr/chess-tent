77bb8b047645de11cc6545a52da315aad1db9191
Tomislav Milanović <tomislav.milanovic@eglas.hr>
2021-05-12 14:57:09 +0200

    #74 Room sync - Refactor syncing flow

--- packages/web/src/modules/state/middleware.ts
+++ packages/web/src/modules/state/middleware.ts
@@ -11,17 +11,12 @@ export const registerMiddleware = (middlewareFunction: Middleware) =>
   middleware.push(middlewareFunction);
 
 export const generalMiddleware: Middleware = store => next => action => {
-  console.log('generalMiddleware');
   if (action.type === SYNC_ACTION) {
-    console.log('generalMiddleware SYNC_ACTION');
     const hasPayload = action.payload !== undefined;
     if (!hasPayload) {
       const { id, type } = action.meta;
-      const entity = store.getState().entities[type][id];
-      const payload = utils.normalize(entity);
-      console.log('action before', action);
-      const actionWithPayload = { ...action, payload };
-      console.log('action after', actionWithPayload);
+      const entity = utils.denormalize(id, type, store.getState().entities);
+      const actionWithPayload = { ...action, payload: entity };
       socket.sendAction(actionWithPayload);
     } else {
       const entity = action.payload;

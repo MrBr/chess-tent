0725592d5f6915c25f53965ce89a31dd242773ad
Tomislav Milanović <tomislav.milanovic@eglas.hr>
2021-05-14 09:16:44 +0200

    #74 Add small code cleanup

--- packages/web/src/modules/state-records/state/_helpers.ts
+++ packages/web/src/modules/state-records/state/_helpers.ts
@@ -3,9 +3,6 @@ import { utils } from '@application';
 import { Entity } from '@chess-tent/models';
 
 export const formatEntityValue = (value: RecordValue) => {
-  // if (value === null) {
-  //   return null;
-  // }
   return Array.isArray(value)
     ? value.map(utils.getEntityId)
     : utils.getEntityId(value);
--- packages/web/src/modules/state-records/state/middleware.ts
+++ packages/web/src/modules/state-records/state/middleware.ts
@@ -2,10 +2,8 @@ import { Middleware, UPDATE_RECORD } from '@types';
 import { updateEntitiesAction } from '../../state/actions';
 
 export const updateRecordEntitiesMiddleware: Middleware = store => next => action => {
-  if (action.type === UPDATE_RECORD) {
-    if (action.payload.value) {
-      store.dispatch(updateEntitiesAction(action.payload.value));
-    }
+  if (action.type === UPDATE_RECORD && action.payload.value) {
+    store.dispatch(updateEntitiesAction(action.payload.value));
   }
   next(action);
 };

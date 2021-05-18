d4f7f680daa9ec6f1d4f334ec11fc348ff04c9ce
Tomislav Milanović <tomislav.milanovic@eglas.hr>
2021-05-18 09:31:10 +0200

    #85 Fix notification update with WS event

--- packages/web/src/modules/notification/state/middleware.tsx
+++ packages/web/src/modules/notification/state/middleware.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Middleware, SEND_NOTIFICATION } from '@types';
-import { services, components } from '@application';
+import { services, components, state } from '@application';
 import { hasNotificationRender } from '../model';
+import { notificationRecordService } from '../service';
 
+const { actions } = state;
+
+const { updateEntities } = actions;
 const { pushToast } = services;
 const { NotificationRender } = components;
 
-export const notificationMiddleware: Middleware = () => next => action => {
+export const notificationMiddleware: Middleware = store => next => action => {
+  if (action.type === SEND_NOTIFICATION) {
+    const notification = action.payload;
+    const { record, updateValue } = notificationRecordService(store);
+    store.dispatch(updateEntities(notification));
+    updateValue([notification.id, ...(record.value || [])]);
+  }
   if (
     action.type === SEND_NOTIFICATION &&
     hasNotificationRender(action.payload.notificationType, 'Toast')
   ) {
-    pushToast(
-      <NotificationRender notification={action.payload} view="Toast" />,
-    );
+    const notification = action.payload;
+    pushToast(<NotificationRender notification={notification} view="Toast" />);
   }
   next(action);
 };

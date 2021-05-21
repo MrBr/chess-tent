9f25b5831fd9cc44d6383352b57af7b6227973eb
Tomislav Milanović <tomislav.milanovic@eglas.hr>
2021-05-21 10:37:50 +0200

    #86 Refactor action type check in updateRecordEntititesMiddleware

--- packages/web/src/modules/state-records/state/middleware.ts
+++ packages/web/src/modules/state-records/state/middleware.ts
@@ -1,11 +1,10 @@
 import { Middleware, UPDATE_RECORD, PUSH_RECORD } from '@types';
 import { updateEntitiesAction } from '../../state/actions';
 
+const updateEntitiesActions = [UPDATE_RECORD, PUSH_RECORD];
+
 export const updateRecordEntitiesMiddleware: Middleware = store => next => action => {
-  if (
-    (action.type === UPDATE_RECORD || action.type === PUSH_RECORD) &&
-    action.payload.value
-  ) {
+  if (updateEntitiesActions.includes(action.type) && action.payload.value) {
     store.dispatch(updateEntitiesAction(action.payload.value));
   }
   next(action);

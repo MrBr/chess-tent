54bb9487d218fc08adfb1d49a22125f987c91d2e
Tomislav Milanović <tomislav.milanovic@eglas.hr>
2021-05-21 10:51:17 +0200

    #86 Specify record actions that update entities

--- packages/web/src/modules/state-records/state/middleware.ts
+++ packages/web/src/modules/state-records/state/middleware.ts
@@ -1,10 +1,12 @@
 import { Middleware, UPDATE_RECORD, PUSH_RECORD } from '@types';
 import { updateEntitiesAction } from '../../state/actions';
 
-const updateEntitiesActions = [UPDATE_RECORD, PUSH_RECORD];
-
 export const updateRecordEntitiesMiddleware: Middleware = store => next => action => {
-  if (updateEntitiesActions.includes(action.type) && action.payload.value) {
+  // Record actions which update entities.
+  if (
+    [UPDATE_RECORD, PUSH_RECORD].includes(action.type) &&
+    action.payload.value
+  ) {
     store.dispatch(updateEntitiesAction(action.payload.value));
   }
   next(action);

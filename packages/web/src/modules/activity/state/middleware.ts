4582a2eae329c7d170817f535e45d524308d38b6
Luka Bracanovic <lbracano@gmail.com>
2021-07-24 22:31:14 +0200

    Fix activity patch middleware

--- packages/web/src/modules/activity/state/middleware.ts
+++ packages/web/src/modules/activity/state/middleware.ts
@@ -6,10 +6,13 @@ export const middleware: Middleware = store => next => action => {
   if (
     action.type === UPDATE_ENTITY &&
     action.meta.type === TYPE_ACTIVITY &&
-    !action.meta.push
+    !action.meta.push &&
+    // Not all UPDATE_ENTITY actions have a patch
+    action.meta.patch
   ) {
     const { id, type, patch } = action.meta;
-    socket.sendAction(state.actions.sendPatchAction(patch, id, type));
+    const patchAction = state.actions.sendPatchAction(patch, id, type);
+    socket.sendAction(patchAction);
   }
   next(action);
 };

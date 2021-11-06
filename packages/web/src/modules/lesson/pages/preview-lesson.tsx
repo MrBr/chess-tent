56289aa87f797b7c3931e5ca90ffffd24cb16d15
Luka Bracanovic <lbracano@gmail.com>
2021-11-06 16:00:23 +0100

    Add lesson overview description and author

--- packages/web/src/modules/lesson/pages/preview-lesson.tsx
+++ packages/web/src/modules/lesson/pages/preview-lesson.tsx
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { hooks, ui, components } from '@application';
 import { Chapter } from '@chess-tent/models';
 import { VariationStep } from '@types';
+
 import ChaptersDropdown from '../components/chapters-dropdown';
 import LessonThumbnail from '../components/thumbnail';
 
@@ -19,7 +20,7 @@ const {
   Headline3,
 } = ui;
 
-const { Layout, Header, Chessboard } = components;
+const { Layout, Header, Chessboard, UserAvatar } = components;
 
 const {
   useParams,
@@ -71,11 +72,26 @@ const PreviewLesson = () => {
         <CardBody className="px-4">
           {lesson && (
             <>
-              <Headline5 className="mt-2 mb-2">{lesson.state.title}</Headline5>
-              <Text fontSize="extra-small" weight={700} className="mb-1">
-                {lesson.difficulty}
-              </Text>
-              <Container className="pl-0 mb-3">
+              <Headline5 className="mt-2 mb-3">{lesson.state.title}</Headline5>
+              <Row noGutters>
+                <Col className="col-auto mr-1">
+                  <UserAvatar user={lesson.owner} size="extra-small" />
+                </Col>
+                <Col>
+                  <Text fontSize="small">{lesson.owner.name}</Text>
+                </Col>
+              </Row>
+              <Container className="pl-0 mb-2 mt-2">
+                <Tag pill variant="primary" className="mr-1">
+                  <Text
+                    fontSize="extra-small"
+                    inline
+                    weight={700}
+                    color="inherit"
+                  >
+                    {lesson.difficulty}
+                  </Text>
+                </Tag>
                 {lesson.tags?.map(({ text, id }) => (
                   <Tag pill key={id} variant="success" className="mr-1">
                     <Text
@@ -91,7 +107,9 @@ const PreviewLesson = () => {
               </Container>
             </>
           )}
-          <Text>{lesson?.state.description}</Text>
+          <Text fontSize="small" className="mb-3">
+            {lesson?.state.description}
+          </Text>
           <Row>
             <Col className="col-6">
               <Headline3 weight={700} className="mb-0 mt-0">
@@ -123,6 +141,12 @@ const PreviewLesson = () => {
     </Container>
   );
 
+  const boardHeader = (
+    <div className="mt-5">
+      <Text onClick={history.goBack}>Back</Text>
+    </div>
+  );
+
   return (
     <Page fluid className="px-0 h-100">
       <Layout
@@ -135,6 +159,7 @@ const PreviewLesson = () => {
           shapes={firstStep.state.shapes}
           footer={null}
           viewOnly
+          header={boardHeader}
         />
       </Layout>
     </Page>

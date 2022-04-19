b5deb79c66cf9ab935020a07c6045792ef06b73c
Luka Bracanovic <lbracano@gmail.com>
2022-04-19 20:35:20 +0200

    Remove bootstrap overrides
    - Using bootstrap variable is a way to go in the long run. However, to be consitent, for now style components localy

--- packages/web/src/modules/ui/ProgressBar.tsx
+++ packages/web/src/modules/ui/ProgressBar.tsx
@@ -3,6 +3,11 @@ import styled from '@chess-tent/styled-props';
 
 const StyledProgressBar = styled(ProgressBar).variant.css`
   height: 2px;
+  
+  &.success.progress-bar {
+    background-color: var(--success-color) !important;
+  }
+  
   .progress-bar {
     border-radius: 10px;
     background-color: var(--primary-color);

5d36e9a8c3020b397de066943e4b00bc5e2ae0fa
Luka Bracanovic <lbracano@gmail.com>
2022-05-04 22:49:18 +0200

    Fix template linking

--- packages/web/src/modules/lesson/pages/templates.tsx
+++ packages/web/src/modules/lesson/pages/templates.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { components, hooks, ui } from '@application';
 
 const { Page, LessonTemplates } = components;
-const { useMyLessons } = hooks;
+const { useMyLessons, useHistory } = hooks;
 const { Headline4, Text, Container } = ui;
 
 const BrowseLessonsPage: React.FC = () => {
   const lessons = useMyLessons();
+  const history = useHistory();
+
+  const openTemplatePage = useCallback(
+    lesson => {
+      history.push(`/lesson/${lesson.id}`);
+    },
+    [history],
+  );
 
   return (
     <Page>
       <Container className="ps-5 pe-5">
         <Headline4 className="m-0 mt-4">Templates ✏️</Headline4>
         <Text className="mb-5">Your training and lesson templates.</Text>
-        {lessons.value && <LessonTemplates lessons={lessons.value} />}
+        {lessons.value && (
+          <LessonTemplates
+            lessons={lessons.value}
+            onLessonClick={openTemplatePage}
+          />
+        )}
       </Container>
     </Page>
   );

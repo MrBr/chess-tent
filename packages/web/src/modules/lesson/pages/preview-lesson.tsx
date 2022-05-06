6b4b4bacaeec7bfec17f04cfa9f900c473dad05f
Luka Bracanovic <lbracano@gmail.com>
2022-05-06 20:19:35 +0200

    Improve preview lesson

--- packages/web/src/modules/lesson/pages/preview-lesson.tsx
+++ packages/web/src/modules/lesson/pages/preview-lesson.tsx
@@ -4,36 +4,24 @@ import { Chapter } from '@chess-tent/models';
 import { VariationStep } from '@types';
 
 import ChaptersDropdown from '../components/chapters-dropdown';
-import LessonThumbnail from '../components/thumbnail';
 import { createLessonActivity } from '../service';
 
-const {
-  Container,
-  Col,
-  Card,
-  Text,
-  Button,
-  Row,
-  Headline5,
-  Tag,
-  Headline3,
-  Absolute,
-} = ui;
+const { Container, Col, Text, Button, Row, Headline4 } = ui;
 
-const { Page, Chessboard, UserAvatar } = components;
+const { Page, Chessboard, UserAvatar, DifficultyLabel, Header, Tags } =
+  components;
+const { Breadcrumbs } = ui;
 
 const {
   useParams,
   useLesson,
   useUserTrainings,
   useActiveUserRecord,
-  useHistory,
   useOpenTraining,
 } = hooks;
 
 const PreviewLesson = () => {
   const { lessonId } = useParams<{ lessonId: string }>();
-  const history = useHistory();
   const { value: lesson } = useLesson(lessonId as string);
   const [chapter, setActiveChapter] = useState<Chapter>();
   const { value: user } = useActiveUserRecord();
@@ -57,7 +45,7 @@ const PreviewLesson = () => {
       return;
     }
     openLesson(ownedLessonActivity);
-  }, [history, ownedLessonActivity, openLesson]);
+  }, [ownedLessonActivity, openLesson]);
 
   if (!lesson) {
     return null;
@@ -67,94 +55,85 @@ const PreviewLesson = () => {
   const firstStep = activeChapter.state.steps[0] as VariationStep;
 
   const sidebar = (
-    <Container>
-      <Card className="mb-4 mt-5 ">
-        {ownedLessonActivity && (
-          <Absolute left={10} top={10}>
-            <Tag bg="light" className="p-2">
-              <Text inline>Owned</Text>
-            </Tag>
-          </Absolute>
-        )}
-        <LessonThumbnail stepRoot={lesson.state.chapters[0]} />
-        <Card.Body className="px-4">
-          <Headline5 className="mt-2 mb-3">{lesson.state.title}</Headline5>
-          <Row className="g-0">
-            <Col className="col-auto me-1">
-              <UserAvatar user={lesson.owner} size="extra-small" />
-            </Col>
-            <Col>
-              <Text fontSize="small">{lesson.owner.name}</Text>
-            </Col>
-          </Row>
-          <Container className="pl-0 mb-2 mt-2">
-            <Tag pill bg="primary" className="me-1">
-              <Text
-                fontSize="extra-small"
-                as="span"
-                weight={700}
-                color="inherit"
-              >
-                {lesson.difficulty}
-              </Text>
-            </Tag>
-            {lesson.tags?.map(({ text, id }) => (
-              <Tag pill key={id} bg="success" className="me-1">
-                <Text
-                  fontSize="extra-small"
-                  inline
-                  weight={700}
-                  color="inherit"
-                >
-                  {text}
-                </Text>
-              </Tag>
-            ))}
-          </Container>
-          <Text fontSize="small" className="mb-3">
-            {lesson?.state.description}
-          </Text>
-          <Row>
-            <Col className="col-6">
-              <Headline3 weight={700} className="mb-0 mt-0">
-                Free
-              </Headline3>
-              <Text color="inherit" fontSize="small">
-                Lifetime access
-              </Text>
-            </Col>
-            <Col className="col-6 d-flex justify-content-end align-items-center">
-              {ownedLessonActivity ? (
-                <Button size="small" onClick={goToLesson} variant="secondary">
-                  Continue
-                </Button>
-              ) : (
-                <Button size="small" onClick={assignLesson}>
-                  Start
-                </Button>
-              )}
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+    <>
+      <Row className="border-bottom">
+        <Col>
+          <Text>Price</Text>
+          <Text>Free</Text>
+        </Col>
+        <Col>
+          {ownedLessonActivity ? (
+            <Button size="small" onClick={goToLesson} variant="secondary">
+              Continue
+            </Button>
+          ) : (
+            <Button size="small" onClick={assignLesson}>
+              Start
+            </Button>
+          )}
+        </Col>
+      </Row>
+      <Row className="border-bottom">
+        <Col>
+          <Text>Description</Text>
+          <Text>{lesson.state.description}</Text>
+        </Col>
+      </Row>
       <ChaptersDropdown
         chapters={lesson.state.chapters}
         activeChapter={activeChapter}
         onChange={chapter => setActiveChapter(chapter)}
       />
-    </Container>
+    </>
   );
 
   const boardHeader = (
-    <div className="mt-5">
-      <Text onClick={history.goBack}>Back</Text>
+    <div className="mt-5 mb-4">
+      <Row className="mb-2">
+        <Col>
+          <DifficultyLabel difficulty={lesson.difficulty} />
+          <Tags tags={lesson.tags} inline className="ms-2" />
+        </Col>
+      </Row>
+      <Headline4>{lesson.state.title}</Headline4>
+      <Row className="g-0 align-items-center">
+        <Col className="col-auto me-2">
+          <Text fontSize="extra-small" className="m-0">
+            Created by
+          </Text>
+        </Col>
+        <Col className="col-auto me-1">
+          <UserAvatar user={lesson.owner} size="extra-small" />
+        </Col>
+        <Col className="col-auto">
+          <Text fontSize="extra-small" className="m-0" weight={400}>
+            <u>{lesson.owner.name}</u>
+          </Text>
+        </Col>
+      </Row>
     </div>
   );
 
+  const pageHeader = (
+    <Header className="border-bottom">
+      <Col>
+        <Breadcrumbs>
+          <Breadcrumbs.Item>Back</Breadcrumbs.Item>
+          <Breadcrumbs.Item>{lesson.state.title}</Breadcrumbs.Item>
+        </Breadcrumbs>
+      </Col>
+      <Col className="col-auto">
+        <Button variant="tertiary" size="extra-small">
+          Share
+        </Button>
+      </Col>
+    </Header>
+  );
+
   return (
-    <Page>
-      <Container fluid>
-        <Row>
+    <Page header={pageHeader}>
+      <Container fluid className="h-100">
+        <Row className="h-100">
           <Col xs={12} sm={7}>
             <Chessboard
               fen={firstStep.state.position as string}
@@ -164,7 +143,9 @@ const PreviewLesson = () => {
               header={boardHeader}
             />
           </Col>
-          <Col sm={5}>{sidebar}</Col>
+          <Col sm={5} className="border-start h-100">
+            {sidebar}
+          </Col>
         </Row>
       </Container>
     </Page>

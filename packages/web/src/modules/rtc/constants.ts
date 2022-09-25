063b45c3d8f447ecf89541eb175d577938ca8627
Luka Bracanovic <lbracano@gmail.com>
2022-09-26 01:02:29 +0200

    Change ice servers confing

--- packages/web/src/modules/rtc/constants.ts
+++ packages/web/src/modules/rtc/constants.ts
@@ -14,7 +14,12 @@ export const DEFAULT_CONSTRAINTS_MOBILE = {
 };
 export const DEFAULT_ICE_SERVERS = [
   {
-    urls: process.env.REACT_APP_TURN_SERVER_URL as string,
+    urls: `turn:${process.env.REACT_APP_TURN_SERVER_IP}:${process.env.REACT_APP_TURN_SERVER_PORT}`,
+    username: process.env.REACT_APP_TURN_SERVER_USERNAME as string,
+    credential: process.env.REACT_APP_TURN_SERVER_PASSWORD as string,
+  },
+  {
+    urls: `stun:${process.env.REACT_APP_TURN_SERVER_IP}:${process.env.REACT_APP_TURN_SERVER_PORT}`,
     username: process.env.REACT_APP_TURN_SERVER_USERNAME as string,
     credential: process.env.REACT_APP_TURN_SERVER_PASSWORD as string,
   },

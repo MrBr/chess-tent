7aaaa975ece0343ed59291b0d24e8933b52dd3c6
Luka Bracanovic <lbracano@gmail.com>
2022-10-19 18:24:33 +0200

    Implement pgn size limit

--- packages/web/src/modules/chessboard/components/pgn-import.tsx
+++ packages/web/src/modules/chessboard/components/pgn-import.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { hoc, ui } from '@application';
 import { FileUploaderProps } from '@types';
 
-const { Modal, Button, Input, FormGroup, Label, Col, Row } = ui;
+const { Modal, Button, Input, FormGroup, Label, Col, Row, Text } = ui;
 const { withFiles } = hoc;
+
+const CharsLimit = 50000;
 
 const PGNModal = withFiles(
   ({
@@ -15,13 +17,20 @@ const PGNModal = withFiles(
     onImport: (pgn: string) => void;
     close: () => void;
   }) => {
+    const [charsCount, setCharsCount] = useState(0);
     const inputRef = useRef<HTMLInputElement>(null);
+    const isValidPgn = charsCount <= CharsLimit;
 
     const handleImport = () => {
-      close();
-      if (inputRef.current) {
-        onImport(inputRef.current.value);
+      if (!inputRef.current) {
+        return;
       }
+      if (inputRef.current.value.length > CharsLimit) {
+        alert('PGN file too big!');
+        return;
+      }
+      onImport(inputRef.current.value);
+      close();
     };
 
     useEffect(() => {
@@ -32,6 +41,7 @@ const PGNModal = withFiles(
         if (!inputRef.current) {
           return;
         }
+        setCharsCount(pgn.length);
         inputRef.current.value = pgn;
       });
     }, [files]);
@@ -56,6 +66,7 @@ const PGNModal = withFiles(
           </Row>
           <FormGroup>
             <Input
+              onChange={e => setCharsCount(e.currentTarget.value.length)}
               as="textarea"
               ref={inputRef}
               rows={20}
@@ -65,6 +76,18 @@ const PGNModal = withFiles(
           </FormGroup>
         </Modal.Body>
         <Modal.Footer>
+          <Col>
+            <Text className="mb-0" fontSize="small">
+              PGN size limit:
+            </Text>
+            <Text
+              color={isValidPgn ? 'black' : 'error'}
+              fontSize="extra-small"
+              className="mb-0"
+            >
+              {charsCount}/{CharsLimit}
+            </Text>
+          </Col>
           <Button onClick={handleImport} size="small">
             Import
           </Button>

2450597bc157d1ab3d69ff46537e1a028b5c56fa
Luka Bracanovic <lbracano@gmail.com>
2022-10-22 17:39:27 +0200

    Use socket hook for activity socket connection

--- packages/web/src/modules/activity/hook.ts
+++ packages/web/src/modules/activity/hook.ts
@@ -1,27 +1,21 @@
-import { useEffect } from 'react';
-import { hooks, records, socket } from '@application';
+import { hooks, records } from '@application';
 import { Hooks, Records } from '@types';
 import { Activity, TYPE_ACTIVITY } from '@chess-tent/models';
 import { RecordHookReturn } from '@chess-tent/redux-record/types';
+
+const { useSocketSubscribe, useRecordInit } = hooks;
 
 export const useActivity: Hooks['useActivity'] = <T extends Activity>(
   activityId: string,
 ): RecordHookReturn<Records<T>['activity']> => {
   const recordKey = `${TYPE_ACTIVITY}-${activityId}`;
-  const record = hooks.useRecordInit(
+
+  const record = useRecordInit(
     records.activity as Records<T>['activity'],
     recordKey,
   );
 
-  useEffect(() => {
-    socket.subscribe(recordKey);
-
-    return () => {
-      // In case activity change from within activity this may not trigger
-      // take care
-      socket.unsubscribe(recordKey);
-    };
-  }, [recordKey]);
+  useSocketSubscribe(recordKey);
 
   return record;
 };

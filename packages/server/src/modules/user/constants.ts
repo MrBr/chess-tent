6341b0e646be55b333bcf84dbf02cd69dbf82118
Luka Bracanovic <lbracano@gmail.com>
2022-11-22 12:39:56 +0100

    Change onboarding messages

--- packages/server/src/modules/user/constants.ts
+++ packages/server/src/modules/user/constants.ts
@@ -5,36 +5,35 @@ const { formatAppLink } = utils;
 export const DEFAULT_SIGNED_PROFILE_URL_EXPIRATION_TIME = 60 * 2; // 2 min
 
 export const introMessagesCoach = [
-  `Hi! I am Luka, founder of Chess Tent. I'll use this opportunity and share with you a few useful tips to get you started.`,
-  `As a coach you can set up public profile by filling in the details at ${formatAppLink(
+  `Hi there! I am Luka, founder of Chess Tent and I am really excited to welcome you on board.`,
+  `The best first step is to fill in your profile details at ${formatAppLink(
     '/me',
   )} page.`,
-  `You can see all coaches at ${formatAppLink('/coaches')} page.`,
-  `Templates are a great place to start, you can prepare lesson and use it later in the training room. Create a new template at ${formatAppLink(
-    '/lesson/new',
-  )} page.`,
-  `Training room offers variety of features such as live interactive board, video conference and works very well with template chapters. Create a new training at ${formatAppLink(
+  `To start practicing create a new training room at ${formatAppLink(
     '?training=true',
+  )} page.`,
+  `To prepare a lesson, create a new template at ${formatAppLink(
+    '/lesson/new',
   )} page.`,
   `You can easily invite your students at ${formatAppLink(
     '?invite=true',
-  )} page. Once they register you'll be able to assign training to them.`,
+  )} page.`,
   `Would love to hear back from you.`,
 ];
 
 export const introMessagesStudent = [
-  `Hi! I am Luka, founder of Chess Tent. I'll use this opportunity and share with you a few useful tips to get you started.`,
-  `Are you looking for a coach? The best place to look for a coach is at ${formatAppLink(
+  `Hi there! I am Luka, founder of Chess Tent and I am really excited to welcome you on board.`,
+  `Chess is more fun with a coach and we've got some of the best you at ${formatAppLink(
     '/coaches',
   )} page.`,
-  `If you prefer learning on your own, find yourself a lesson at ${formatAppLink(
+  `If you prefer learning on your own, find a lesson at ${formatAppLink(
     '/lessons',
   )} page.`,
-  `Hope you'll find the platform useful, I'd love to hear your feedback.`,
+  `Would love to hear back from you.`,
 ];
 
 export const introMessagesInvitedStudent = [
-  `Hi! I am Luka, founder of Chess Tent. I'll use this opportunity and share with you a few useful tips to get you started.`,
+  `Hi there! I am Luka, founder of Chess Tent and I am really excited to welcome you on board.`,
   `You can reach out to your coach at ${formatAppLink('/me/coaches')} page.`,
   `If you're interested in chess lessons take a look at ${formatAppLink(
     '/lessons',

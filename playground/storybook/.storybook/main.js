455a6130ec4711f1cd26e5e832c7aec3d2b2ab6e
Aleks Markovic <aleks.markovic182@gmail.com>
2023-07-11 16:37:01 +0200

    Change storybook config

--- playground/storybook/.storybook/main.js
+++ playground/storybook/.storybook/main.js
@@ -63,8 +63,8 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-links',
     '@storybook/addon-interactions',
   ],
   framework: '@storybook/react',
--- playground/storybook/.storybook/preview.ts
+++ playground/storybook/.storybook/preview.ts
@@ -1,6 +1,11 @@
 import './env';
+import { addDecorator } from '@storybook/react';
 
 import { loaders as webLoaders } from './web';
+
+addDecorator(story => {
+  return story();
+});
 
 // preview.js
 export const loaders = [...webLoaders];

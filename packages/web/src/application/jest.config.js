7af48507bccbed129c97f0fd8ccdc78fb1dfabd1
Luka Bracanovic <lbracano@gmail.com>
2023-07-22 11:15:25 +0200

    Fix web tests

--- packages/web/src/application/jest.config.js
+++ packages/web/src/application/jest.config.js
@@ -3,5 +3,5 @@ const sharedConfig = require('../../jest.config.js');
 module.exports = {
   ...sharedConfig,
   roots: ['<rootDir>/src/application'],
-  setupFilesAfterEnv: ['<rootDir>/src/application/setup.jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/application/env.jest.ts', '<rootDir>/src/application/setup.jest.ts'],
 };

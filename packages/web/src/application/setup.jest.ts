797ebf6d16ac48746c599db768953d064f942209
Aleks Markovic <aleks.markovic182@gmail.com>
2023-07-24 10:37:48 +0200

    Add package required for Zoom testing

--- packages/web/src/application/setup.jest.ts
+++ packages/web/src/application/setup.jest.ts
@@ -1,6 +1,10 @@
 import application from '@application';
+import { Crypto } from '@peculiar/webcrypto';
 
 import '../modules';
+
+// required for ZoomSdk
+global.crypto = new Crypto();
 
 beforeAll(() => application.init());
 

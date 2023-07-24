b1ec607811abd6b1b3106b2782d95e5b3d3ef55a
Aleks Markovic <aleks.markovic182@gmail.com>
2023-07-24 10:38:23 +0200

    Remove outdated warning

--- packages/web/src/application/setup.jest.ts
+++ packages/web/src/application/setup.jest.ts
@@ -7,6 +7,3 @@ import '../modules';
 global.crypto = new Crypto();
 
 beforeAll(() => application.init());
-
-console.warn('See jest.config. - TLDR; upgrade to jest 28.0.6 when possible');
-// module.exports = async () => application.init();

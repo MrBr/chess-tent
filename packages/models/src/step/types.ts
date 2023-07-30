43da49757955eed47bfa1f7946b274d545d1f6f7
Vedran Abramovic <vedran.abramovic@gmail.com>
2023-07-30 22:08:35 +0200

    Implement save, get and delete methods

--- packages/models/src/step/types.ts
+++ packages/models/src/step/types.ts
@@ -1,4 +1,7 @@
 import { Subject } from '../subject';
+import { User } from '../user';
+import { Tag } from '../tag';
+import { Difficulty } from '../lesson';
 
 export type StepType = 'description' | 'variation' | 'move' | 'exercise';
 
@@ -6,15 +9,27 @@ export const TYPE_STEP = 'steps';
 
 export interface Step<T extends {} = {}, K extends StepType = StepType> {
   id: string;
+  docId?: string;
+  owner: User;
   type: typeof TYPE_STEP;
   stepType: K;
+  difficulty?: Difficulty;
+  tags?: Tag[];
+  users?: User[];
+  published?: boolean;
   state: { steps: Step[] } & T;
 }
 
 export interface NormalizedStep {
   id: Step['id'];
+  docId?: Step['docId'];
+  owner: User['id'];
   type: Step['type'];
   stepType: Step['stepType'];
+  difficulty: Step['difficulty'];
+  tags: Step['tags'][];
+  published?: Step['published'];
+  users?: User['id'][];
   state: Step['state'] & {
     steps: Step['id'][];
   };
---
+++ packages/server/src/modules/step/model.ts
@@ -0,0 +1,78 @@
+import {Difficulty, NormalizedStep, TYPE_STEP, TYPE_TAG, TYPE_USER} from '@chess-tent/models';
+import {db} from "@application";
+import {Schema} from "mongoose";
+import {lessonSchema} from "../lesson/model";
+import {lessonAdapter} from "../lesson/adapter";
+
+// could be called StepEntity as entities are established concepts in the persistence context
+export interface DepopulatedStep {
+  id: NormalizedStep['id'];
+  docId: NormalizedStep['docId'];
+  type: NormalizedStep['type'];
+  owner: NormalizedStep['owner'];
+  state: NormalizedStep['state'];
+  difficulty?: NormalizedStep['difficulty'];
+  tags: NormalizedStep['tags'];
+  published: NormalizedStep['published'];
+  users: NormalizedStep['users'];
+  v?: number;
+}
+
+const stepSchema = db.createSchema<DepopulatedStep>(
+    {
+      docId: {
+        type: String
+      } as unknown as DepopulatedStep['docId'],
+      type: {
+        type: String,
+        ref: TYPE_STEP
+      } as unknown as DepopulatedStep['type'],
+      owner: {
+        type: String,
+        ref: TYPE_USER
+      } as unknown as DepopulatedStep['owner'],
+      state: {
+        type: Schema.Types.Mixed,
+        required: true,
+      } as unknown as DepopulatedStep['state'],
+      difficulty: {
+        type: String,
+        enum: Object.keys(Difficulty),
+        required: true,
+        index: true,
+      } as unknown as DepopulatedStep['difficulty'],
+      tags: [
+        {
+          type: String,
+          ref: TYPE_TAG,
+        } as unknown,
+      ] as DepopulatedStep['tags'],
+      published: {
+        type: Boolean
+      } as unknown as DepopulatedStep['published'],
+      users: [
+        {
+          type: String,
+          ref: TYPE_USER,
+        } as unknown,
+      ] as DepopulatedStep['users'],
+      v: {
+        type: Schema.Types.Number,
+        default: 1
+      } as unknown as number,
+    },
+    { minimize: false }
+);
+
+stepSchema.index({'state.id': 'text'})
+
+// do we need adapter before schema needs to be modified?
+// db.applyAdapter(stepSchema, stepAdapter);
+
+const StepModel = db.createModel<DepopulatedStep>(
+    TYPE_STEP,
+    stepSchema
+)
+
+export { stepSchema, StepModel }
+

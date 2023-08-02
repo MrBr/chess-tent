1d474116d2df17e5045d2decf7af48a9bb1d2031
Aleks Markovic <aleks.markovic182@gmail.com>
2023-08-02 10:40:08 +0200

    Move Zoom Provider component

--- packages/web/src/modules/zoom/index.ts
+++ packages/web/src/modules/zoom/index.ts
@@ -1,6 +1,6 @@
 import application from '@application';
 
-import ZoomProvider from './components/zoom-provider';
+import ZoomProvider from './providers/zoom-provider';
 import ZoomActivityView from './components/zoom-activity-view';
 import ZoomHostControl from './components/controls/host-control';
 import ZoomGuestControl from './components/controls/guest-control';

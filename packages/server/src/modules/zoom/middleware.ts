d1d9bff74f0fa79ffbef73959b0c9063bf6a5c70
Aleks Markovic <aleks.markovic182@gmail.com>
2023-08-05 09:59:29 +0200

    Catch zoom endpoints error to avoid server crash

--- packages/server/src/modules/zoom/middleware.ts
+++ packages/server/src/modules/zoom/middleware.ts
@@ -2,19 +2,19 @@ import { MiddlewareFunction } from '@types';
 import * as zoomService from './service';
 
 const authorizeUserByCode: MiddlewareFunction = async (req, res, next) => {
-  const data = await zoomService.authorizeUserByCode(
-    res.locals.zoom.code,
-    res.locals.zoom.redirectUri,
-  );
+  const data = await zoomService
+    .authorizeUserByCode(res.locals.zoom.code, res.locals.zoom.redirectUri)
+    .catch(next);
+
   res.locals.zoomAuthorization = data;
   next();
 };
 
 const generateSignature: MiddlewareFunction = async (req, res, next) => {
-  const data = await zoomService.generateSignature(
-    res.locals.zoom.meetingNumber,
-    res.locals.zoom.role,
-  );
+  const data = await zoomService
+    .generateSignature(res.locals.zoom.meetingNumber, res.locals.zoom.role)
+    .catch(next);
+
   res.locals.zoomSignature = data;
   next();
 };

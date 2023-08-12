d40c3476c07ae54f9aba9026aa04919a0b57ade4
Vedran Abramovic <vedran.abramovic@gmail.com>
2023-08-12 19:31:05 +0200

    Add test that verifies that the step module is registered with the application context

---
+++ packages/server/src/modules/step/__tests__/get-step-route.spec.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+
+import application from '@application';
+
+describe('GET /step/:stepId', () => {
+  beforeAll(() => application.start());
+  afterAll(() => application.stop());
+  it('should return forbidden status', function (done) {
+    request(application.service.router)
+      .get(process.env.API_BASE_PATH + '/step/812376819764')
+      .set('Accept', 'application/json')
+      .expect(401, done);
+  });
+});

8324885fabaea6839a858c2269be671585a94f36
Vedran Abramovic <vedran.abramovic@gmail.com>
2023-08-25 00:35:11 +0200

    Test coaches step management

--- packages/server/src/modules/step/__tests__/get-step-route.spec.ts
+++ packages/server/src/modules/step/__tests__/get-step-route.spec.ts
@@ -1,31 +1,64 @@
 import request from 'supertest';
 
 import application from '@application';
-import { TYPE_USER, User } from '@chess-tent/models';
+import { generateApiToken, generateCoach } from '../../../application/tests';
+import { Step, TYPE_STEP, User } from '@chess-tent/models';
 import { v4 as uuid } from 'uuid';
-import { seedUser } from '../../../application/tests';
 
 describe('POST /step/save', () => {
   beforeAll(() => application.test.start());
   afterAll(() => application.stop());
 
   it('should create new step', function (done) {
-    const user: User = {
-      id: uuid(),
-      nickname: 'coach1',
-      name: 'Coachy McCoachFace',
-      email: 'coach1@chesstent.com',
-      type: TYPE_USER,
-      active: true,
-      coach: true,
-      password: 'testpassword1',
-    } as User;
-
-    seedUser(user);
-    const token = application.service.generateApiToken(user);
+    const coach: User = generateCoach();
+    const token: String = generateApiToken(coach);
 
     request(application.service.router)
       .post(process.env.API_BASE_PATH + '/step/save')
+      .set('Cookie', [`token=${token}`])
+      .expect(200, done);
+  });
+});
+
+describe('Coach should be able to manage created step', () => {
+  beforeAll(() => application.test.start());
+  afterAll(() => application.stop());
+
+  let stepId: String;
+  let coach: User;
+  let token: String;
+
+  it('should create new step', function (done) {
+    coach = generateCoach();
+    token = generateApiToken(coach);
+    stepId = uuid();
+
+    request(application.service.router)
+      .post(process.env.API_BASE_PATH + '/step/save')
+      .set('Cookie', [`token=${token}`])
+      .set('Accept', 'application/json')
+      .set({
+        id: stepId,
+        type: TYPE_STEP,
+        state: {},
+      } as Step)
+      .expect(200)
+      .end((err, response) => {
+        if (err) {
+          console.error('Error:', err);
+          return done(err);
+        }
+
+        console.log('Response Body:', response.body);
+
+        done();
+      });
+  });
+
+  it('should be able to get step', function (done) {
+    request(application.service.router)
+      .get(process.env.API_BASE_PATH + `/step/${stepId}`)
+      .set('Accept', 'application/json')
       .set('Cookie', [`token=${token}`])
       .expect(200, done);
   });

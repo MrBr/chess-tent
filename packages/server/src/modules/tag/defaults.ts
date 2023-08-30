e899971167e9bada4c49d592c2e6426558ea9cf6
Vedran Abramovic <vedran.abramovic@gmail.com>
2023-08-30 17:16:37 +0200

    Revert commented snippet

--- packages/server/src/modules/tag/defaults.ts
+++ packages/server/src/modules/tag/defaults.ts
@@ -14,11 +14,9 @@ const defaultTags = [
   'Mate',
 ].map(text => ({ text, id: service.generateIndex() }));
 
-console.warn('Move to migration');
-// TODO - move to migration
-// TagModel.findOne({ text: defaultTags[0].text }).then(result => {
-//   if (result) {
-//     return;
-//   }
-//   TagModel.insertMany(defaultTags);
-// });
+TagModel.findOne({ text: defaultTags[0].text }).then(result => {
+  if (result) {
+    return;
+  }
+  TagModel.insertMany(defaultTags);
+});

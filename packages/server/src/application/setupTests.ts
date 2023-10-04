a6c627d0231d06d4c1b623222748ecc39a0f197c
Aleks Markovic <aleks.markovic182@gmail.com>
2023-10-04 17:28:14 +0200

    Silence console errors while testing

--- packages/server/src/application/setupTests.ts
+++ packages/server/src/application/setupTests.ts
@@ -5,3 +5,11 @@ import '../modules';
 
 process.env.TOKEN_SECRET = 'test_secret';
 beforeAll(() => application.init());
+
+beforeAll(async () => {
+  jest.spyOn(console, 'error').mockImplementation(jest.fn());
+});
+
+afterAll(() => {
+  jest.resetAllMocks();
+});
